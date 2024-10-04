39dce2b913131f2d1184115a70310b7bc4b4b84e
Dan Berglund <dbergl@hotmail.com>
2024-10-04 11:49:28 -0700

    Add tank sensors (#4)
    * Add tank levels as reported by the G12. Add additional RV-C spec item for 0BFC1 G12_TANK_LEVEL_SENSOR
    
    * Add g12 tank sensor levels and allow for a minimum_change value before sending

---
+++ rvc2mqtt/entity/g12_tank_level_sensor.py
@@ -0,0 +1,119 @@
+"""
+Tank Level Sensor
+
+Copyright 2022 Sean Brogan
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+import queue
+import logging
+import json
+import struct
+from rvc2mqtt.mqtt import MQTT_Support
+from rvc2mqtt.entity import EntityPluginBaseClass
+
+class TankLevelSensor_TANK_STATUS(EntityPluginBaseClass):
+    FACTORY_MATCH_ATTRIBUTES = {"type": "g12_tank_level", "name": "G12_TANK_LEVEL"}
+
+    """ Provide specific tank level values using DGN G12_TANK_LEVEL_SENSOR
+        These are broadcast by the g12 unit on 0BFC1
+
+    """
+
+    def __init__(self, data: dict, mqtt_support: MQTT_Support):
+        self.id = "g12tanklevel-0BFC1-i" + str(data["instance"])
+        super().__init__(data, mqtt_support)
+        self.Logger = logging.getLogger(__class__.__name__)
+
+        # RVC message must match the following to be this device
+        self.rvc_match_status = {"name": "G12_TANK_LEVEL_SENSOR", "instance": data['instance']}
+        self.tank_level = 65535
+        self.Logger.debug(f"Must match: {str(self.rvc_match_status)}")
+
+        self.name = data['instance_name']
+        self.instance = data['instance']
+        self.diff_min = data.get('minimum_change', 1)
+
+        self.device = {"manufacturer": "RV-C",
+                       "via_device": self.mqtt_support.get_bridge_ha_name(),
+                       "identifiers": self.unique_device_id,
+                       "name": self.name,
+                       "model": "RV-C Tank from G12_TANK_LEVEL_SENSOR"
+                       }
+
+
+    def process_rvc_msg(self, new_message: dict) -> bool:
+        """ Process an incoming message and determine if it
+        is of interest to this object.
+
+        If relevant - Process the message and return True
+        else - return False
+        """
+        # For now only match the status message.
+
+        if self._is_entry_match(self.rvc_match_status, new_message):
+            self.Logger.debug(f"Msg Match Status: {str(new_message)}")
+
+            # These events happen a lot.  Lets filter down to when the value changed by more than 10
+            if abs(new_message["tank_level"] - self.tank_level) >= int(self.diff_min):
+                self.tank_level = new_message['tank_level']
+                self.mqtt_support.client.publish(
+                        self.status_topic, self.tank_level, retain=True)
+                
+            
+            return True
+        return False
+
+    def initialize(self):
+        """ Optional function 
+        Will get called once when the object is loaded.  
+        RVC canbus tx queue is available
+        mqtt client is ready.  
+
+        This can be a good place to request data    
+        """
+        # request dgn report - this should trigger the tanks to report
+        # dgn = 0BFC1 which is actually  C1 BF 00 <instance> 00 00 00 00
+        self.Logger.debug("Sending Request for DGN")
+        data = struct.pack("<BBBBBBBB", int("0xC1", 0), int(
+            "0xBF", 0), 0, self.instance, 0, 0, 0, 0)
+        self.send_queue.put({"dgn": "EAFF", "data": data})
+
+        # send auto discovery info
+        self._send_ha_mqtt_discovery_info()
+
+    
+    def _send_ha_mqtt_discovery_info(self):
+
+        # produce the HA MQTT discovery config json
+        config = {"name": self.name,
+                  "state_topic": self.status_topic,
+                  "qos": 1, "retain": False,
+                  "state_class": "measurement",
+                  "value_template": '{{value}}',
+                  "unique_id": self.unique_device_id,
+                  "device": self.device}
+        config.update(self.get_availability_discovery_info_for_ha())
+
+        config_json = json.dumps(config)
+
+        ha_config_topic = self.mqtt_support.make_ha_auto_discovery_config_topic(
+            self.unique_device_id, "sensor")
+
+        # publish info to mqtt
+        self.mqtt_support.client.publish(
+            ha_config_topic, config_json, retain=True)
+
