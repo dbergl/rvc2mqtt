aec2590034c72e145603dfe5ead9a003e567fdda
Dan Berglund <dan@fullbeaker.com>
2025-05-18 20:47:01 -0700

    WIP

--- rvc2mqtt/entity/timberline.py
+++ rvc2mqtt/entity/timberline.py
@@ -425,14 +425,13 @@ class hvac_TIMBERLINE(EntityPluginBaseClass):
 
         elif self._is_entry_match(self.rvc_timberline_proprietary, new_message):
             self.Logger.debug(f"Msg Match Status: {str(new_message)}")
+            self.Logger.error(f"Msg Match Status: {str(new_message)}")
             if new_message["message_type"] == "81": #0x81 Timberline 1.5 Extension Error codes clear command
                 # This is the command. Eat message so it doesn't show up as unhandled.
                 self.Logger.debug(f"Msg Match Command: {str(new_message)}")
-                processed = True
             elif new_message["message_type"] == "83": #0x81 Timberline 1.5 Extension command
                 # This is the command. Eat message so it doesn't show up as unhandled.
                 self.Logger.debug(f"Msg Match Command: {str(new_message)}")
-                processed = True
             elif new_message["message_type"] == "84": #0x84 Timberline 1.5 Extension status message
                 if new_message["solenoid"] != self._solenoid:
                     self._solenoid = new_message["solenoid"]
@@ -451,18 +450,19 @@ class hvac_TIMBERLINE(EntityPluginBaseClass):
                     self.mqtt_support.client.publish(
                         self.tank_temperature_topic, new_message["tank_temperature"], retain=True)
                     self.mqtt_support.client.publish(
-                        self.tank_temperaturef_topic,round(float(self._convert_c_to_f(new_message["tank_temperature"]))), retain=True)
+                        self.tank_temperaturef_topic,round(float(
+                            self._convert_c_to_f(new_message["tank_temperature"]))), retain=True)
                 if new_message["heater_temperature"] != self._heater_temperature:
                     self._heater_temperature = new_message["heater_temperature"]
                     self.mqtt_support.client.publish(
                         self.heater_temperature_topic, new_message["heater_temperature"], retain=True)
                     self.mqtt_support.client.publish(
-                        self.heater_temperaturef_topic,round(float(self._convert_c_to_f(new_message["heater_temperature"]))), retain=True)
+                        self.heater_temperaturef_topic,round(float(
+                            self._convert_c_to_f(new_message["heater_temperature"]))), retain=True)
                 if new_message["fan_manual_speed"] != self._fan_manual_speed:
                     self._fan_manual_speed = new_message["fan_manual_speed"]
                     self.mqtt_support.client.publish(
                         self.fan_manual_speed_topic, new_message["fan_manual_speed"], retain=True)
-                processed = True
             elif new_message["message_type"] == "85": #0x85 Timberline 1.5 Timers
                 if new_message["system_timer"] != self._system_timer:
                     self._system_timer = new_message["system_timer"]
@@ -476,7 +476,6 @@ class hvac_TIMBERLINE(EntityPluginBaseClass):
                     self._pump_override_timer = new_message["pump_override_timer"]
                     self.mqtt_support.client.publish(
                         self.pump_override_timer_topic, new_message["pump_override_timer"], retain=True)
-                processed = True
             elif new_message["message_type"] == "86": #0x86 Timberline 1.5 Heater info
                 self.Logger.warning(f"")
             elif new_message["message_type"] == "87": #0x87 Timberline 1.5 Panel info
@@ -486,7 +485,6 @@ class hvac_TIMBERLINE(EntityPluginBaseClass):
             elif new_message["message_type"] == "89": #0x81 Timberline 1.5 Extension command
                 # This is the command. Eat message so it doesn't show up as unhandled.
                 self.Logger.debug(f"Msg Match Command: {str(new_message)}")
-                processed = True
             elif new_message["message_type"] == "8A": #0x8A Timberline 1.5 Timers Setup status
                 self.Logger.warning(f"")
 
